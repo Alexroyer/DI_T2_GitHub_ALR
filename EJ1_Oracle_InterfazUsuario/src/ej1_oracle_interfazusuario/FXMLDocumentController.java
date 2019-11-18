862cc6ee4f642dcc843ce855b52a7f29c8c9584b
Alexroyer <56017365+Alexroyer@users.noreply.github.com>
2019-11-18 19:34:20 +0100

    Subida de ejercicios

---
+++ EJ1_Oracle_InterfazUsuario/src/ej1_oracle_interfazusuario/FXMLDocumentController.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ej1_oracle_interfazusuario;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+
+/**
+ *
+ * @author Aleja
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML private Text actiontarget;
+    
+    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+        actiontarget.setText("Sign in button pressed");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
